2221819f676b1184e0c335827ad11fc482c3cfab
Jean Paul NISHIMIRWE <55387431+Nishimirwe@users.noreply.github.com>
2021-06-25 09:31:40 +0200

    Update Stack.py

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -2,7 +2,13 @@ class stack:
     def __init__(self):
         self.li=list()
         self.s=-1
-        self.reset=-1010101
+        self.reset=-10101
+
+    #isEmpty
+    def isEmpty(self):
+        if self.s<0:
+            return True
+        return False
 
     #add
     def push(self,value):
@@ -11,27 +17,38 @@ class stack:
 
     #peek
     def peek(self):
-        return self.li[self.s]
-
+        if not self.isEmpty():
+            return self.li[self.s]
+        return "Empty List", -1
     #pop
     def pop(self):
-        x=self.li[self.s]
-        self.li[self.s]=self.reset
-        self.s-=1
-        return x
+        if not self.isEmpty():
+            x=self.li[self.s]
+            self.li[self.s]=self.reset
+            self.s-=1
+            return x
+        return "Empty List", -1
 
     #view
     def display(self):
-        for i in range(self.s+1):
-            print(self.li[i],"::",end='')
-        print()
-        print("Size: ",self.s+1)
+        if not self.isEmpty():
+            for i in range(self.s+1):
+                print(self.li[i],"::",end='')
+            print()
+            print("Size: ",self.s+1)
+        else:
+            print("Empty list")
 
+    #destroy
+    def destroy(self):
+        self.s=-1
+        self.li.clear()
 st=stack()
 st.push(12)
 st.push(43)
 st.push(50)
 st.push(55)
+st.destroy()
 st.display()
 print(st.peek()," ",st.pop())
 st.display()
