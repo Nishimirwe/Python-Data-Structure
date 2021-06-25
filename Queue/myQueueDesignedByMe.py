ba97efe24c7699e6c45f83293f6406494dde817b
Jean Paul NISHIMIRWE <55387431+Nishimirwe@users.noreply.github.com>
2021-06-25 09:06:20 +0200

    Update myQueueDesignedByMe.py

--- Queue/myQueueDesignedByMe.py
+++ Queue/myQueueDesignedByMe.py
@@ -12,26 +12,25 @@ class Queue:
 
     #def pop()
     def pop(self):
-        x=self.li[min]
-        self.li[min]=self.reset
+        x=self.li[self.min]
+        self.li[self.min]=self.reset
         self.min+=1
-        self.s-=1
         return x
 
     #def front
     def front(self):
-        return self.li[min]
+        return self.li[self.min]
     
     #back element
     def back(self):
-        return self.li[s]
+        return self.li[self.s]
 
     #diplay()
     def display(self):
         for i in range(self.min,self.s+1):
             print(self.li[i],"::",end='')
         print()
-        print("Size: ",self.s+1)
+        print("Size: ",self.s+1-self.min)
 
 q=Queue()
 q.push(12)
@@ -39,4 +38,6 @@ q.push(15)
 q.push(20)
 q.push(25)
 q.push(30)
+q.display()
+print("Pop: ",q.pop()," Front: ",q.front()," Back: ",q.back())
 q.display()
