514d53264bd8fcf69adf613d5e224a4edd12f947
Jean Paul NISHIMIRWE <55387431+Nishimirwe@users.noreply.github.com>
2021-07-05 13:27:57 +0200

    Update LinearSearch.py

--- Searching Algorithms/LinearSearch.py
+++ Searching Algorithms/LinearSearch.py
@@ -1,11 +1,13 @@
 #Linear search, we loop through an array and check if a value we want exist
 #We use loop
 
+#The function returns the index of tha value if found, otherwise return -1
+
 def LinearSearch(names,value):
-    for i in names:
-        if(i==value):
-            return "Found"
-    return "Not Found"
+    for i in range(len(names)):
+        if(names[i]==value):
+            return i
+    return -1
 
 names=["Paul","Jean","Jane","Rashid","Japhet"]
-print(LinearSearch(names,"Paula"))
+print(LinearSearch(names,"Paul"))
