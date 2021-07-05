055dc4925b648af36dac265c71bca587e5b5516f
Jean Paul NISHIMIRWE <55387431+Nishimirwe@users.noreply.github.com>
2021-07-05 13:53:40 +0200

    Update BinarySearch.py

--- Searching Algorithms/BinarySearch.py
+++ Searching Algorithms/BinarySearch.py
@@ -14,6 +14,18 @@ def BinarySearch(arr,start,end,value):
         else:
             return BinarySearch(arr,start,mid-1,value)
 
+def BinarySearchLoop(arr,start,end,value):
+    while 1:
+        mid=(start+end)//2
+        if value==arr[mid]:
+            return mid
+        elif end==start:
+            return -1
+        elif value>arr[mid]:
+            start=mid+1
+        else:
+            end=mid-1
 
-li=[1,2,4,10,15]
-print(BinarySearch(li,0,len(li)-1,1765))
+li=[1,2,3,4,5,6,7,8,9]
+print(BinarySearchLoop(li,0,len(li)-1,9))
+print(BinarySearch(li,0,len(li)-1,6))
