ad340d5e02895a4af7182cfd79dbf01f2b82329a
Jean Paul NISHIMIRWE <55387431+Nishimirwe@users.noreply.github.com>
2021-07-05 10:26:03 +0200

    Add files via upload

---
+++ Sorting ALgorithms/insertionSortAlgorithm.py
@@ -0,0 +1,15 @@
+#In insertiokn sorting algorithm, we move through out an array
+#put it in its appropriate place.
+
+def insertion(li):
+    for j in range(len(li)):
+        t=j-1
+        k=j
+        while li[k]<li[t] and k>0:
+            li[k],li[t]=(li[t],li[k])
+            k-=1
+            t-=1
+    return li
+
+arr=[5,3,4,7,2,8,6,9,1]
+print(insertion(arr))
