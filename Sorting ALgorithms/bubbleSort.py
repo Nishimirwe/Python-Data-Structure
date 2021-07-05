eae1d3356a6908a45fe9dc2e0469678257839bef
Jean Paul NISHIMIRWE <55387431+Nishimirwe@users.noreply.github.com>
2021-07-05 07:26:57 +0200

    Add files via upload

---
+++ Sorting ALgorithms/bubbleSort.py
@@ -0,0 +1,9 @@
+def sort(li):
+    for i in range(len(li)-1):
+        for j in range(i+1,len(li)):
+            if li[i]>li[j]:
+                li[i],li[j]=(li[j],li[i])
+    return li
+
+li=[70,10,80,30,20,40,60,50,90]
+print(sort(li))
