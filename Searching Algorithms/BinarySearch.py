41362b6883be26078e74b198a72aade7fb73966a
Jean Paul NISHIMIRWE <55387431+Nishimirwe@users.noreply.github.com>
2021-07-05 13:25:27 +0200

    Add files via upload

---
+++ Searching Algorithms/BinarySearch.py
@@ -0,0 +1,16 @@
+
+def BinarySearch(arr,start,end,value):
+    mid=int((start+end)/2)
+    if arr[mid]==value:
+        return mid
+    elif end==start:
+        return -1
+    else:
+        if arr[mid]<value:
+            return BinarySearch(arr,mid+1,end,value)
+        else:
+            return BinarySearch(arr,start,mid-1,value)
+
+
+li=[1,2,4,10,15]
+print(BinarySearch(li,0,len(li)-1,15))
